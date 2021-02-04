2c37881e53a78d221ea8482c4088d10b9237b993
Vincent Baaij <vnbaaij@outlook.com>
2021-02-04 14:01:39 +0100

    Update to .NET 5

--- MeterMonitor/Program.cs
+++ MeterMonitor/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.Extensions.Hosting;
 
 using MeterMonitor.Reader;
 using MeterMonitor.Configuration;
+using System;
+using Azure.Identity;
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
 
 namespace MeterMonitor
 {
@@ -27,14 +30,14 @@ namespace MeterMonitor
 
                         using var store = new X509Store(StoreLocation.CurrentUser);
                         store.Open(OpenFlags.ReadOnly);
-                        var certs = store.Certificates
-                            .Find(X509FindType.FindByThumbprint,
-                                builtConfig["AZUREADCERTTHUMBPRINT"], false);
+                        
+                        X509Certificate2Collection certs = store.Certificates
+                        .Find(X509FindType.FindByThumbprint,
+                            builtConfig["AzureADCertThumbprint"], false);
 
-                        config.AddAzureKeyVault(
-                            $"https://{builtConfig["KEYVAULTNAME"]}.vault.azure.net/",
-                            builtConfig["AZUREADAPPLICATIONID"],
-                            certs.OfType<X509Certificate2>().Single());
+                        config.AddAzureKeyVault(new Uri($"https://{builtConfig["KEYVAULTNAME"]}.vault.azure.net/"),
+                                                new ClientCertificateCredential(builtConfig["AZUREADDIRECTORYID"], builtConfig["AZUREADAPPLICATIONID"], certs.OfType<X509Certificate2>().Single()),
+                                                new KeyVaultSecretManager());
 
                         store.Close();
                     }
